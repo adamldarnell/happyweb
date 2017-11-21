be84bb2ed7e428741c4c58df903f1783e5f04065
Adam Darnell <adamdarnell@MacBook-Pro.local>
2017-11-20 21:03:51 -0600

    added proposal

---
+++ HappywebProposal.py
@@ -0,0 +1,33 @@
+- Happy Web Read Me File - 
+
+For this project we will mainly be working with 2 datasets: 
+
+1. 2017 World Happiness Report Data
+2. % of Individuals Using the Internet (Country)
+
+Purpose of Project: Compare internet usage data with overall happiness by Country. 
+Can too much internet usage be detrimental to emotional health?
+
+With the data within these datasets we will be making various comparisons on internet use/access and general happiness/
+emotional well-being.
+
+Some comparisons may include:
+
+- Happiness rank compared to Internet users
+
+- Broadband Subscriptions compared to Happiness Score
+
+- Freedom Score compared to Internet users
+
+- Health/Life Expectancy compared to etc...
+
+The data included in the CSV that we will be referencing includes: family/freedom scores, Government trust scores,
+economy, health/ Life expectancy, Generosity 
+
+Possible Sources: "https://www.kaggle.com/unsdsn/world-happiness/data"
+         "http://www.itu.int/en/ITU-D/Statistics/Pages/default.aspx"
+         "http://www.itu.int/en/ITU-D/Statistics/Pages/stat/default.aspx"
+
+
+
+
